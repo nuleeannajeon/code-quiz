0d7506ee6b6e4fe62a2437f3e1dd8fcceec5906f
Anna Jeon <Anna@Annas-MacBook-Air.local>
2020-05-27 22:05:00 -0400

    Question/AnswerCorrectioe JS

--- script.js
+++ script.js
@@ -1,19 +1,68 @@
-var startButton = document.querySelector("#start-btn");
-var questionContainer = document.querySelector("#question-container");
+var startContainer = document.querySelector(".start-container");
+var questionContainerEl = document.querySelector("#question-container");
+var questionEl = document.querySelector("#question");
+var answerButtonEl = document.querySelector("#answer-btn");
 
-startButton.addEventListener('click', startQuiz)
+let shuffledQuestions, currentQuestionIndex
+
+document.querySelector('#answer-btn').addEventListener('click', () => {
+    currentQuestionIndex++
+    nextQuestion() });
+document.querySelector('#answer-btn').addEventListener('click', answerCorrection);
+
 
 function startQuiz() {
     console.log ( `You have started the quiz. Good luck!` );
-    document.querySelector(".start-container").classList.add('hide');
-    questionContainer.classList.remove('hide');
+    startContainer.classList.add('hide');
+    shuffledQuestions = questionSet.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    questionContainerEl.classList.remove('hide');
 
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
 } 
 
+function nextQuestion() {
+    // reset ();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);  
+}
+
+// function reset() {
+//     clearStatusClass(document.body);
+//     while (answerButtonEl.firstChild) {
+//         answerButtonEl.removeChild(answerButtonEl.firstChild);
+//     }
+// }
+// NEED TO FIX IT MORE IN ORDER TO HIDE THE PREVIOUS ANSWER BUTTON
+
+function showQuestion(question) {
+    questionEl.innerText = question.question;
+    question.answer.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn')
+            if (answer.correct) {
+                button.dataset.correct = answer.correct
+            }
+        answerButtonEl.appendChild(button);
+    })}
+   
+function answerCorrection(event) {  
+    const selectedBtn = event.target;
+    const correct = selectedBtn.dataset.correct;
+    if (correct==="true"){
+        console.log (" hey you got correct answer ");
+        document.querySelector('#correct-answer').classList.remove('vision');
+    } else {
+        console.log("WRONG!!!!!")
+        document.querySelector('#wrong-answer').classList.remove('vision');
+    }
+}
 
 
 
-var questions = [
+
+
+var questionSet = [
     { question: " What is the standard language used to create web pages? ",
         answer: [
             { text: "HyperText Markup Language (HTML)", correct: true },
